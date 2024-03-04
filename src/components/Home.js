b87493dc9e7a94d1128e548ee0cda9f713e264f8
Pranav Rao <pranavrao541@gmail.com>
2024-03-04 19:56:50 +0530

    update: home-page-updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../components/style.css";
 import "../components/responsive.css";
-import pranav from "../components/images/pranav.jpg";
+import pranav from "../components/images/pranav.png";
 import resume from "../components/Pranav_Rao_Resume.pdf";
 
 const Home = () => {
@@ -13,19 +13,19 @@ const Home = () => {
       <div className="container">
         <div className="row home-content">
           <div className="hero-text col-md-6">
-            <h2>
+            <h2 className="home-title">
               Hi, I'm <span>Pranav Rao</span>
             </h2>
-            <h3>
+            <h3 className="home-sub-title">
               I'm a <span>Frontend Web Developer</span>
             </h3>
-            <p>
+            <p className="major-one-liner">
               Computer Science Engineer <b>[B. Tech]</b>
             </p>
-            <p>I Design and Develop Dynamic Websites.</p>
+            <p className="major-one-liner">I Design and Develop Dynamic Websites.</p>
             <a href={resume} download="Pranav's_Resume" rel="noreferrer">
               <button className="download-button mb-4">
-                Download CV <i className="bi bi-download"></i>
+                Download CV <i class="bi bi-file-earmark-arrow-down-fill"></i>
               </button>
             </a>
             <div className="home-socials">
