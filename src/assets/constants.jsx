d155b4359a82ddbda78d072164a003f1cb9a1cb9
Pranav Rao <pranavrao541@gmail.com>
2024-08-03 00:56:31 +0530

    update: cnhages for Projects

--- src/assets/constants.jsx
+++ src/assets/constants.jsx
@@ -99,4 +99,20 @@ const TickIcon = () => (
   </svg>
 );
 
-export { LinkedInIcon, GitHubIcon, FacebookIcon, InstagramIcon, CopyIcon, TickIcon };
+const ExpandIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 25 25"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M4 12h16M12 4l8 8-8 8" />
+  </svg>
+);
+
+export { LinkedInIcon, GitHubIcon, FacebookIcon, InstagramIcon, CopyIcon, TickIcon, ExpandIcon };
