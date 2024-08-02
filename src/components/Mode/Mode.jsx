e648669b9e71c88fea7cbf59bbaa95e82b5ba345
Pranav Rao <pranavrao541@gmail.com>
2024-08-03 04:12:27 +0530

    update: UI changes

--- src/components/Mode/Mode.jsx
+++ src/components/Mode/Mode.jsx
@@ -11,10 +11,6 @@ const Mode = () => {
   const toggleMode = () => {
     setIsDarkMode(!isDarkMode);
     document.documentElement.classList.toggle("dark", !isDarkMode);
-    // const main = document.querySelector("main");
-    // localStorage.setItem("mode", isDarkMode);
-    // main.classList.toggle("bg-gray-950");
-    // main.classList.toggle("text-gray-100");
   };
 
   const handleScroll = useCallback(() => {
@@ -48,14 +44,14 @@ const Mode = () => {
     <motion.div
       initial={{ opacity: 0, y: -10 }}
       animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : -10 }}
-      className="min-w-fit fixed top-4 left-4 flex gap-2 z-50"
+      className="min-w-fit fixed bottom-2 right-2 flex gap-2 z-50"
     >
       <button
         onClick={toggleMode}
-        className="px-4 py-2 rounded bg-gray-950 dark:bg-gray-500 hover:bg-blue-700 text-gray-100 dark:text-gray-950 transition-all duration-300 flex gap-1.5 items-center justify-center font-medium"
+        className="px-4 py-2 rounded bg-gray-900 dark:bg-gray-950 hover:bg-blue-700 dark:hover:bg-blue-700 text-gray-100 transition-all duration-300 flex gap-1.5 items-center justify-center font-medium"
       >
         {isDarkMode ? <SunIcon /> : <MoonIcon />}
-        {isDarkMode ? "Light" : "Dark"}
+        <span className="hidden lg:block">{isDarkMode ? "Light" : "Dark"}</span>
       </button>
     </motion.div>
   );
