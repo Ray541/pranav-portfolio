a3c168f62fe9d0d57c45591c7bbeed48ad9556a2
Pranav Rao <pranavrao541@gmail.com>
2024-08-02 16:37:59 +0530

    update: added Home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <section id="home"></section>;
+};
+
+export default Home;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header/Header";
+// Sections
+import Home from "../components/Home/Home";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <main className="w-full h-full"></main>
+      <main className="w-full h-full">
+        <Home />
+      </main>
     </>
   );
 };
