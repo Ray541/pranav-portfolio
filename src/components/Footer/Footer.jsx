896eca64d94f977bb6524a6721aed08867114f5d
Pranav Rao <pranavrao541@gmail.com>
2024-08-03 11:31:22 +0530

    update: changes in UI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
       <div className="flex flex-col items-center md:flex-row justify-between gap-1">
         <p className="text-2xl flex items-center justify-center gap-1 font-black">
           Developed by <span className="text-blue-600">Ray</span>
+          <span className="hover:scale-125 transition-all duration-150 ease-in-out">❤️</span>
         </p>
         <p className="text-lg md:mt-0">Copyright &copy; {new Date().getFullYear()} Ray</p>
       </div>
