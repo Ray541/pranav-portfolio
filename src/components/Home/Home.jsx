c682de1efcffa8977ff85f439fe95bb6fcd277e9
Pranav Rao <pranavrao541@gmail.com>
2024-08-25 13:45:45 +0530

    update: used react icons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import pranavImage from "../../assets/pranav.jpg";
 import resume from "../../assets/Pranav's_Resume.pdf";
-import { LinkedInIcon, GitHubIcon, FacebookIcon, InstagramIcon } from "../../assets/constants";
+import { SiLinkedin, SiGithub, SiFacebook, SiInstagram } from "react-icons/si";
+import { MdOutlineFileDownload } from "react-icons/md";
 import { motion } from "framer-motion";
 
 const Home = () => {
@@ -31,24 +32,18 @@ const Home = () => {
             target="_blank"
             className="bg-gray-900 dark:bg-gray-950 hover:bg-blue-700 dark:hover:bg-blue-700 focus:bg-blue-700 dark:focus:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 transition-all duration-50 flex items-center justify-center gap-1 shadow-md hover:translate-y-1 max-w-fit cursor-pointer"
           >
-            <svg
-              className="fill-current w-4 h-4 mr-2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-            </svg>
+            <MdOutlineFileDownload className="text-xl" />
             <span className="tracking-wide">Download CV</span>
           </a>
         </div>
-        <div className="mt-5 flex gap-5 items-center justify-center lg:justify-start">
+        <div className="mt-5 flex gap-6 items-center justify-center lg:justify-start">
           <a
             href="https://www.linkedin.com/in/pranav-rao-09a79b231/"
             target="_blank"
             rel="noreferrer"
             className="hover:text-blue-700 focus:text-blue-700"
           >
-            <LinkedInIcon />
+            <SiLinkedin className="text-xl" />
           </a>
           <a
             href="https://github.com/Ray541"
@@ -56,7 +51,7 @@ const Home = () => {
             rel="noreferrer"
             className="hover:text-blue-700 focus:text-blue-700"
           >
-            <GitHubIcon />
+            <SiGithub className="text-xl" />
           </a>
           <a
             href="https://www.facebook.com/pranav.rao.338"
@@ -64,7 +59,7 @@ const Home = () => {
             rel="noreferrer"
             className="hover:text-blue-700 focus:text-blue-700"
           >
-            <FacebookIcon />
+            <SiFacebook className="text-xl" />
           </a>
           <a
             href="https://www.instagram.com/pranav_rao0504"
@@ -72,7 +67,7 @@ const Home = () => {
             rel="noreferrer"
             className="hover:text-blue-700 focus:text-blue-700"
           >
-            <InstagramIcon />
+            <SiInstagram className="text-xl" />
           </a>
         </div>
       </div>
