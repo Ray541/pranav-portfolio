670baa88e8d4a64a138aa54dba9ae6d0325aab2a
Pranav Rao <pranavrao541@gmail.com>
2024-11-19 16:58:12 +0530

    update: added time and changed the animation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,28 @@
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const Footer = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000); // Update every second
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <footer className="p-4">
       <div className="flex flex-col items-center md:flex-row justify-between gap-1">
-        <p className="text-2xl flex items-center justify-center gap-1 font-black">
-          Developed by <span className="text-blue-600">Ray</span>
+        <p className="text-2xl flex items-center justify-center gap-1">
+          Developed by <span className="text-blue-600">Pranav</span>
           <motion.span
             className="hover:scale-125 transition-all duration-150 ease-in-out"
             animate={{
-              scale: [1, 1.3, 1],
+              scale: [0.5, 1.25, 1],
             }}
             transition={{
-              duration: 1.5,
               repeat: Infinity,
               repeatType: "loop",
             }}
@@ -20,7 +30,8 @@ const Footer = () => {
             ❤️
           </motion.span>
         </p>
-        <p className="text-lg md:mt-0">Copyright &copy; {new Date().getFullYear()} Pranav</p>
+        <p className="text-blue-500 font-semibold">{time.toLocaleTimeString()}</p>
+        <p className="text-lg md:mt-0">Copyright &copy; {new Date().getFullYear()}</p>
       </div>
     </footer>
   );
