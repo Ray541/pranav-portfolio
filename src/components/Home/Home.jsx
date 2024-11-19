a4fa4b6ed21f2048cf5c81859d1ddfdee4743c38
Pranav Rao <pranavrao541@gmail.com>
2024-11-19 15:38:51 +0530

    update: removed image from home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,27 @@
-import pranavImage from "../../assets/pranav.jpg";
+// import pranavImage from "../../assets/pranav.jpg";
 import resume from "../../assets/Pranav's_Resume.pdf";
 import { SiLinkedin, SiGithub, SiFacebook, SiInstagram } from "react-icons/si";
 import { MdOutlineFileDownload } from "react-icons/md";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <section
       id="home"
-      className="h-screen flex items-center justify-center md:gap-10 lg:gap-0 md:justify-evenly flex-wrap p-3 md:flex-row"
+      className="h-screen flex items-center justify-center md:gap-10 lg:gap-0 md:justify-evenly flex-wrap p-3 md:flex- bg-gray-200 dark:bg-gray-950"
     >
-      <div className="flex justify-center flex-col flex-wrap text-center lg:text-start gap-2 sm:gap-1">
-        <h1 className="text-4xl font-semibold md:text-6xl lg:text-7xl">
+      <div className="flex items-center justify-center flex-col flex-wrap text-center lg:text-start gap-2 sm:gap-1 lg:w-1/2">
+        <h1 className="text-4xl font-semibold text-gray-700 dark:text-gray-200 md:text-6xl lg:text-7xl">
           Hi, I'm{" "}
           <span className="font-black text-blue-600 text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
             Pranav Rao
           </span>
           .
         </h1>
-        <h2 className="text-2xl font-semibold md:text-3xl lg:text-4xl">
-          I am a <span className="font-bold text-blue-600">Frontend Web Developer !!</span>
-        </h2>
-        <p className="text-lg mt-1">Passionate frontend developer started my carrier in Web Dev.</p>
-        <p className="text-lg">
-          I specialize in using <span className="font-bold text-blue-600 text-xl">React.js</span> to
+        {/* <h2 className="text-2xl font-semibold text-gray-700 md:text-3xl lg:text-4xl">
+          I am a <span className="text-5xl font-bold text-blue-600">Frontend Web Developer</span>
+        </h2> */}
+        <p className="text-center text-lg font-semibold mt-1">Passionate <span className="font-bold text-blue-600 text-xl">Frontend Developer</span> started my carrier in Web Dev. I specialize in using <span className="font-bold text-blue-600 text-xl">React.js</span> to
           build modern, dynamic and efficient websites.
         </p>
         <div className="flex items-center justify-center lg:block">
@@ -71,13 +69,13 @@ const Home = () => {
           </a>
         </div>
       </div>
-      <motion.div
-        className="hidden sm:block w-[300px] lg:w-[450px] overflow-hidden"
-        animate={{ y: [0, -5, 0], x: [5, -5, 0, -5, 5] }}
+      {/* <motion.div
+        className="hidden md:block w-[300px] lg:w-[450px] overflow-hidden"
+        animate={{ y: [5, -5, 5], x: [5, -5, 0, -5, 5] }}
         transition={{ duration: 5, repeat: Infinity }}
       >
         <img className="w-full h-full" src={pranavImage} alt="Pranav" />
-      </motion.div>
+      </motion.div> */}
     </section>
   );
 };
