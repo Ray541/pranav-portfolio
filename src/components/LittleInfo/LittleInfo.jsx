f4dfdc3cfb2ed9832507e3267d637918a42df6aa
Pranav Rao <pranavrao541@gmail.com>
2024-11-19 16:23:53 +0530

    fix: animation bug

--- src/components/LittleInfo/LittleInfo.jsx
+++ src/components/LittleInfo/LittleInfo.jsx
@@ -2,12 +2,14 @@ import { motion } from "framer-motion";
 const LittleInfo = () => {
   return (
     <section
-      id="project"
+      id="little-info"
       className="px-5 py-5 text-3xl cursor-none bg-gray-100 dark:bg-gray-900 dark:border-t-2 dark:border-b-2"
     >
       <motion.div
-        animate={{ x: [0, 20, 0] }}
-        transition={{ duration: 2, repeat: Infinity }}
+        animate={{ 
+          x: [0, -10, 0, 10, 0] // Middle → Left → Middle → Right → Middle
+         }}
+        transition={{ duration: 5, repeat: Infinity }}
         className="flex items-center justify-center gap-5"
       >
         <p className="drop-shadow-2xl">🏍️</p>
