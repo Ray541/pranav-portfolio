36ee8dad71a05823b89aa0b7566588f29971a49e
pranavrao <pranav@geopageconsultants.in>
2024-12-27 18:30:57 +0530

    update: added useLenis hook

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -12,8 +12,12 @@ import Footer from "../components/Footer/Footer";
 import Mode from "../components/Mode/Mode";
 import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import CursorFollower from "../components/CursorFollower/CursorFollower";
+// Hook
+import useLenis from "../hooks/useLenis";
 
 const Layout = () => {
+  useLenis();
+
   return (
     <div className="duration-300 overflow-hidden">
       <CursorFollower />
