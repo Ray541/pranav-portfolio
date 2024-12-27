fd820cf8f388834f6e6d508721b8e859fac6ba8f
pranavrao <pranav@geopageconsultants.in>
2024-12-27 12:14:15 +0530

    update: added the utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6366F1",
+      },
+    },
   },
   plugins: [],
 };
