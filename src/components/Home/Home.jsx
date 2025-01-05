acd97a67ad0c4d01fd5c4a8a751859d53a5b84e8
Pranav Rao <pranavrao541@gmail.com>
2025-01-05 16:40:04 +0530

    update: added animation to name and scroll-icon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,6 +14,14 @@ const Home = () => {
   const introSubTitleAnimate = { opacity: 1, y: 0 };
   const introSubTitleTransition = { duration: 1, ease: "circInOut" };
 
+  const introNameInitial = { opacity: 0, };
+  const introNameAnimate = { opacity: 1, };
+  const introNameTransition = { duration: 1, ease: "circInOut" };
+
+  const introScrollIconInitial = { opacity: 0, top: -50 };
+  const introScrollIconAnimate = { opacity: 1, top: 0 };
+  const introScrollIconTransition = { duration: 1, ease: "circInOut" };
+
   return (
     <section
       id="home"
@@ -21,18 +29,26 @@ const Home = () => {
     >
       <div className="w-[200px] h-[200px] rounded-full bg-primary -z-10 absolute top-10 left-0 blur-3xl opacity-90"></div>
       <div className="w-[150px] h-[150px] rounded-full bg-primary -z-10 absolute bottom-20 right-0 blur-3xl opacity-50"></div>
-      <div className="hidden absolute top-5 right-10 z-10 lg:flex lg:items-center lg:justify-center lg:flex-col lg:gap-1">
+      <motion.div 
+      initial={introScrollIconInitial}
+      animate={introScrollIconAnimate}
+      transition={introScrollIconTransition}
+      className="hidden absolute top-5 right-10 z-10 lg:flex lg:items-center lg:justify-center lg:flex-col lg:gap-1">
         <div className="w-[1px] h-5 bg-gray-400 dark:bg-gray-400"></div>
         <BiSolidMouse className="text-xl text-gray-700 dark:text-gray-100" />
         <div className="w-[1px] h-8 bg-gray-400 dark:bg-gray-400"></div>
-      </div>
-      <div className="hidden sm:block sm:w-52 overflow-hidden absolute z-10 bottom-3 left-0 sm:left-10 hover:scale-90 transition-all duration-150">
+      </motion.div>
+      <motion.div 
+      initial={introNameInitial}
+      animate={introNameAnimate}
+      transition={introNameTransition} 
+      className="hidden sm:block sm:w-52 overflow-hidden absolute z-10 bottom-3 left-0 sm:left-10 hover:scale-90 transition-all duration-150">
         <img
           className="w-[100%] h-[100%] backdrop-filter drop-shadow-lg"
           src={pranavImage}
           alt=""
         />
-      </div>
+      </motion.div>
       <div className="w-full h-full flex items-center justify-center backdrop-filter backdrop-blur-3xl p-3 md:p-0">
         <div className="flex items-center justify-center flex-col flex-wrap text-center lg:text-start gap-2 sm:gap-1 lg:w-3/4">
           <motion.h1
