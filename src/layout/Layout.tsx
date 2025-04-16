3f7db18b4d935148b9dbef612ad0b7ae39304534
Pranav Rao <pranavrao541@gmail.com>
2025-04-16 19:59:10 +0530

    feat: add mode toggle

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header/Header";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Main } from "./Main/Main";
 import Home from "@/components/Home/Home";
 
@@ -6,6 +7,7 @@ const Layout = () => {
   return (
     <>
       <Header />
+      <ModeToggle className="absolute bottom-10 right-10 rounded-full" />
       <Main>
         <Home />
       </Main>
