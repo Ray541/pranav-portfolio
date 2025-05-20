7bee76e78b748a38a54d63df9aab1100b36eb9e2
pranavrao <pranav@geopageconsultants.in>
2025-05-20 12:40:26 +0530

    feat: add footer

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,6 +7,7 @@ import Projects from "@/components/Projects/Projects";
 import LittleInfo from "@/components/LittleInfo/LittleInfo";
 import Experience from "@/components/Experience/Experience";
 import Contact from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
 
 const Layout = () => {
   return (
@@ -21,6 +22,7 @@ const Layout = () => {
         <Experience />
         <Contact />
       </Main>
+      <Footer />
     </>
   );
 };
