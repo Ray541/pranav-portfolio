563deb6f730d735b92ae102ba5caf1c5c71bc067
pranavrao <pranav@geopageconsultants.in>
2025-05-21 15:35:32 +0530

    update: change div to nav for

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -84,7 +84,7 @@ const Header = () => {
       ref={headerRef}
       className="fixed top-10 z-5 w-full flex justify-center items-center pointer-events-none"
     >
-      <div className="pointer-events-auto flex gap-6 px-3 py-3 bg-background/60 border backdrop-blur-xs backdrop-saturate-150 rounded-full shadow-md md:px-6">
+      <nav className="pointer-events-auto flex gap-6 px-3 py-3 bg-background/60 border backdrop-blur-xs backdrop-saturate-150 rounded-full shadow-md md:px-6">
         {HEADER_ITEMS.map(item => (
           <a
             key={item}
@@ -97,7 +97,7 @@ const Header = () => {
             {item}
           </a>
         ))}
-      </div>
+      </nav>
     </header>
   );
 };
