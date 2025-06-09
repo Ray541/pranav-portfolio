11d45b60902ce6149240a9b4f32b0e5961d83d9b
pranavrao <pranav@geopageconsultants.in>
2025-06-09 18:07:14 +0530

    feat: background transparent

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -8,7 +8,7 @@ const Section = ({ className, sectionName, children }: SectionProps) => {
   return (
     <section
       id={sectionName}
-      className={`w-full max-w-7xl container bg-background flex items-center justify-center flex-col ${className}`}
+      className={`w-full max-w-7xl container bg-transparent flex items-center justify-center flex-col ${className}`}
     >
       {children}
     </section>
