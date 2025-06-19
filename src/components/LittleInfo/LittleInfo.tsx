a1759ea651a0fc23ddebf0a67f9e0a76a7939852
pranavrao <pranav@geopageconsultants.in>
2025-06-19 12:41:38 +0530

    feat: change descriptions and added anime

--- src/components/LittleInfo/LittleInfo.tsx
+++ src/components/LittleInfo/LittleInfo.tsx
@@ -4,17 +4,18 @@ import { handleCursorEnter, handleCursorLeave } from "@/utils/gsapUtils";
 
 const LittleInfo = () => {
   const data = [
-    { icon: "🏍️", description: "Apachi RTR 160 4V — Rupali" },
-    { icon: "⚽", description: "PlayeD FootbalL..." },
-    { icon: "🌆", description: "EveninG PersoN" },
-    { icon: "📸", description: "CapturE TimE in FrameS" },
-    { icon: "🎧", description: "Well who doesn't like MusicS" },
+    { icon: "🏍️", description: "Apache RTR 160 4V — Rupali." },
+    { icon: "⚽", description: "Football Fever — Passionate about Game and Discipline." },
+    { icon: "🌇", description: "Evenings Soul — My Time of Day." },
+    { icon: "📸", description: "Framing Time — Framing Moments that Matter." },
+    { icon: "🎧", description: "Music — Fixed Mixtapes." },
+    { icon: "🧧", description: "Anime Addict — Funny How it relates Reality." },
   ];
 
   return (
     <div className="w-full border-t border-b border-border border-dashed flex items-center justify-center py-7">
       <Section sectionName="little-info" className="h-auto w-full">
-        <div className="w-full flex items-center justify-center flex-row flex-wrap gap-5 text-3xl">
+        <div className="w-full flex items-center justify-center flex-row flex-wrap gap-5 text-2xl md:text-3xl">
           {data.map((d, index) => (
             <TooltipProvider key={index}>
               <Tooltip>
