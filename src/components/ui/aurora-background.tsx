82a7ed75a80d97bf2de0d49ffb538d40033724b1
pranavrao <pranav@geopageconsultants.in>
2025-07-11 12:01:44 +0530

    update: add responsve animation style sizing

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -49,7 +49,7 @@ export const AuroraBackground = ({
       {...props}
     >
       <div
-        className="absolute inset-0 overflow-hidden hidden md:block"
+        className="absolute inset-0 overflow-hidden"
         style={
           {
             "--aurora":
@@ -77,7 +77,7 @@ export const AuroraBackground = ({
             `after:animate-aurora pointer-events-none absolute -inset-[10px] [background-image:var(--white-gradient),var(--aurora)] [background-size:300%,_200%] [background-position:50%_50%,50%_50%] opacity-50 blur-[10px] invert filter will-change-transform [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)] [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)] [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)] after:absolute after:inset-0 after:[background-image:var(--white-gradient),var(--aurora)] after:[background-size:200%,_100%] after:[background-attachment:fixed] after:mix-blend-difference after:content-[""] dark:[background-image:var(--dark-gradient),var(--aurora)] dark:invert-0 after:dark:[background-image:var(--dark-gradient),var(--aurora)]`,
 
             showRadialGradient &&
-              `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_50%)]`
+              `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)] md:[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_50%)]`
           )}
         />
       </div>
