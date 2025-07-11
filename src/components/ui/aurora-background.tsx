0e6cff0255db306651feed57d92e99644e039db3
pranavrao <pranav@geopageconsultants.in>
2025-07-11 11:49:44 +0530

    perf: optimize aurora background by removing extra div

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -79,15 +79,7 @@ export const AuroraBackground = ({
             showRadialGradient &&
               `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_50%)]`
           )}
-        >
-          <div
-            className={cn(
-              `pointer-events-none absolute -inset-[10px] [background-image:var(--white-gradient),var(--aurora)] [background-size:300%,_200%] [background-position:50%_50%,50%_50%] opacity-50 blur-[10px] filter will-change-transform [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)] [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)] [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)] dark:[background-image:var(--dark-gradient),var(--aurora)]`,
-              showRadialGradient &&
-                `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`
-            )}
-          />
-        </div>
+        />
       </div>
       {children}
     </div>
