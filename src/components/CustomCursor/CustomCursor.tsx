14f5d2b00a32aaed66b5d9aca5c814b8e50c78aa
pranavrao <pranav@geopageconsultants.in>
2025-07-15 18:40:46 +0530

    feat: on click and relase gsap animation

--- src/components/CustomCursor/CustomCursor.tsx
+++ src/components/CustomCursor/CustomCursor.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import { getCurrentScale } from "../../utils/gsapUtils"; // Adjust path as needed
 
 gsap.registerPlugin(useGSAP);
 
@@ -8,19 +9,44 @@ const CustomCursor = () => {
   const cursorRef = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
+    // Mouse movement
     const move = (e: MouseEvent) => {
       gsap.to(cursorRef.current, {
-        x: e.clientX - 12 / 2,
-        y: e.clientY - 12 / 2,
-        duration: 0.25, // Mouse lerp effect
+        x: e.clientX - 6,
+        y: e.clientY - 6,
+        duration: 0.25,
+        ease: "power2.out",
+      });
+    };
+
+    // On mouse Click
+    const handleMouseDown = () => {
+      const currentScale = getCurrentScale();
+      gsap.to(cursorRef.current, {
+        scale: currentScale > 1 ? currentScale * 0.9 : 0.75, // Click scale is different when the cursor is hovered and not hovered
+        duration: 0.15,
+        ease: "power2.out",
+      });
+    };
+
+    // On mouse Release
+    const handleMouseUp = () => {
+      const hoverScale = getCurrentScale();
+      gsap.to(cursorRef.current, {
+        scale: hoverScale,
+        duration: 0.15,
         ease: "power2.out",
       });
     };
 
     window.addEventListener("mousemove", move);
+    window.addEventListener("mousedown", handleMouseDown);
+    window.addEventListener("mouseup", handleMouseUp);
 
     return () => {
       window.removeEventListener("mousemove", move);
+      window.removeEventListener("mousedown", handleMouseDown);
+      window.removeEventListener("mouseup", handleMouseUp);
     };
   }, []);
 
--- src/utils/gsapUtils.ts
+++ src/utils/gsapUtils.ts
@@ -1,10 +1,33 @@
 import gsap from "gsap";
 
-const scaleCursor = (scale: number) => {
-  gsap.to("#cursor", { scale, duration: 0.3 });
+let currentScale = 1;
+
+const scaleCursor = (scale: number, animate = true) => {
+  currentScale = scale;
+  gsap.to("#cursor", {
+    scale,
+    duration: animate ? 0.3 : 0,
+    ease: "power2.out",
+  });
 };
 
+/**
+ * Function to set enter cursor scale
+ * @param scale gasp Scale size to be applied
+ * @returns Transformed cursor with updated scale
+ */
 const handleCursorEnter = (scale: number) => scaleCursor(scale);
+
+/**
+ * Function to set exit cursor scale
+ * @returns Transformed cursor to normal scale
+ */
 const handleCursorLeave = () => scaleCursor(1);
 
-export { scaleCursor, handleCursorEnter, handleCursorLeave };
+/**
+ * Function to get the current scale of the cursor
+ * @returns Current scale of the cursor
+ */
+const getCurrentScale = () => currentScale;
+
+export { scaleCursor, handleCursorEnter, handleCursorLeave, getCurrentScale };
